d464690cb2f89734005fd4f3862ea8ffcd929114
felipeuntill <felipeassuncao.net@gmail.com>
2016-04-28 00:07:21 -0300

    inital coding

---
+++ src/option-extender.js
@@ -0,0 +1,10 @@
+// Summary:
+//   Option Extender, the best way to manage default ans extended properties in javascript.
+// Arguments:
+//   - current(required): current properties, normaly they should be your pre configured properties.
+//   - extended(required): the extension, keys from this object will be replaced in your pre configured properties.
+var optionExtender = function (current, extended) {
+    
+    
+    
+};
