591dbaa9a64d8b9595bb936a15416dfb81515fc1
Felipe Assunção <felipeassuncao.net@gmail.com>
2016-04-30 21:22:39 -0300

    fixing module issue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 // Exporting the calendar module.
 var optionExtender = require('./optionExtender');
-
-var config = { 
-    name : 'config name'  
-};
-var extended = {  
-    name : 'extended name' 
-};
-
-console.log(optionExtender(config, extended));
+module['exports'] = optionExtender
