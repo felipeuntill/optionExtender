004f43392f19e63243a123c95179e8369714cb76
Felipe Assunção <felipeassuncao.net@gmail.com>
2016-05-06 21:56:34 -0300

    Update optionExtender.js

--- src/optionExtender.js
+++ src/optionExtender.js
@@ -3,7 +3,7 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 // Summary:
-//   Option Extender, the best way to manage default ans extended properties in javascript.
+//   optionExtender, the best way to manage default and extended properties in javascript.
 // Arguments:
 //   - current(required): current properties, normaly they should be your pre configured properties.
 //   - extended(required): the extension, keys from this object will be replaced in your pre configured properties.
